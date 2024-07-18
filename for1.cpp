9d8adce8e383d46ecc064742da091266ee1075e3
Deepikakarunanithi <deepika.k@rapidcare.ai>
2024-07-18 17:22:41 +0530

    Create for1.cpp

---
+++ for1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main ()
+{
+
+  for( int i= 0 ; i< 109 ; i++)
+    {
+
+      std::cout<<"[" <<i <<"]" <<" Deeps !"<<std::endl;
+      
+    }
+ std::cout<<"Loop end well !"<<std::endl;
+
+  return 0;
+}
